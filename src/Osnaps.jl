3b33eeb9bad091f8db04788d42e45f887f2bbc41
yctai <brianyjtai1994@gmail.com>
2021-07-23 23:50:35 +0800

    init Osnaps.jl

---
+++ src/Osnaps.jl
@@ -0,0 +1,16 @@
+module Osnaps
+
+const VecI  = AbstractVector # Input  Vector
+const VecO  = AbstractVector # Output Vector
+const VecB  = AbstractVector # Buffer Vector
+const VecIO = AbstractVector # In/Out Vector
+const MatI  = AbstractMatrix # Input  Matrix
+const MatO  = AbstractMatrix # Output Matrix
+const MatB  = AbstractMatrix # Buffer Matrix
+const MatIO = AbstractMatrix # In/Out Matrix
+
+const 𝚷 = 2.0 * π
+
+fcall(fn::Function, x::VecI) = fn(x)
+
+end # module
