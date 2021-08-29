db7194e088336bbb4c7aecacff4aa054f6d09ac4
yctai <brianyjtai1994@gmail.com>
2021-08-29 10:24:02 +0800

    Edit: Variational Bayesian inference (VBI)
    VBI now uses Levenberg-Marquardt method with geodesic acceleration.

--- src/Bayes/variantional_bayesian.jl
+++ src/Bayes/variantional_bayesian.jl
@@ -22,37 +22,6 @@ function varbayes_update!(Λn::MatIO, Λy::MatI, Λ0::MatI, Js::MatI, As::MatB)
     return nothing
 end
 
-varbayes_lmdamp(μ::Real, ρ::Real) = ρ < 0.9367902323681495 ? μ * (1.0 - cubic(2.0 * ρ - 1.0)) : μ / 3.0
-
-function varbayes_lmdamp(Λn::MatI)
-    μ = -Inf
-    @inbounds for i in axes(Λn, 1)
-        μ = max(μ, Λn[i,i])
-    end
-    return μ
-end
-
-# compute: hn ← As' * ks - Λ0 * (mn - m0)
-function varbayes_lmstep!(hn::VecB, Λ0::MatI, Δm::VecB, As::MatB, ks::VecI)
-    symv!('U', 1.0, Λ0, Δm,  0.0, hn) # hn ← Λ0 * Δm
-    gemv!('T', 1.0, As, ks, -1.0, hn) # hn ← As' * ks - hn
-    return nothing
-end
-
-# compute: mn ← mn + hn, (J' * Λy * J + Λ0 + μI) * hn = (J' * Λy * k - Λ0 * Δm)
-function varbayes_lmstep!(mn::VecIO, ms::VecI, hn::VecI, Λf::MatB, μ::Real)
-    @simd for i in eachindex(mn)
-        @inbounds Λf[i,i] += μ # Λf ← Λn + μI
-    end
-    _, cholesky_state = potrf!('L', Λf)
-    trsv!('L', 'N', 'N', Λf, hn)
-    trsv!('L', 'T', 'N', Λf, hn)
-    @simd for i in eachindex(mn)
-        @inbounds mn[i] = ms[i] + hn[i]
-    end
-    return cholesky_state
-end
-
 function varbayes_energy!(Λy::MatI, ks::VecI, Λ0::MatI, Δm::VecB, Λf::MatB, Λe::MatB)
     trsm!('L', 'L', 'N', 'N', 1.0, Λf, Λe)
     trsm!('L', 'L', 'T', 'N', 1.0, Λf, Λe)
@@ -60,7 +29,7 @@ function varbayes_energy!(Λy::MatI, ks::VecI, Λ0::MatI, Δm::VecB, Λf::MatB, 
 end
 
 # barebone version
-function varbayes(m0::VecI, Λ0::MatI, ys::VecI, Λy::MatI, mo::Function; τ::Real=1e-3, itmax::Int=100)
+function varbayes(m0::VecI, Λ0::MatI, ys::VecI, Λy::MatI, mo::Function; τ::Real=1e-3, α::Real=0.1, itmax::Int=100)
     np = length(m0) # dims of params
     nd = length(ys) # dims of data
 
@@ -81,13 +50,16 @@ function varbayes(m0::VecI, Λ0::MatI, ys::VecI, Λy::MatI, mo::Function; τ::Re
     Λe = Matrix{Float64}(undef, np, np)
     Λf = Matrix{Float64}(undef, np, np)
 
+    invα1 = inv(α)
+    invα2 = abs2(invα1)
+
     unsafe_copy!(ms, m0)
     unsafe_copy!(Λs, Λ0)
 
     fcall!(ks, mo, ms) # use current best `ms` to compute `ks`
     gcall!(Js, mo, ms) # use current best `ms` to compute `Js`
     varbayes_update!(Λn, Λy, Λ0, Js, As)
-    μ = varbayes_lmdamp(Λn) * τ
+    μ = diagmax(Λn) * τ
     ν = 2.0
 
     varbayes_update!(ks, ys)
@@ -100,14 +72,40 @@ function varbayes(m0::VecI, Λ0::MatI, ys::VecI, Λy::MatI, mo::Function; τ::Re
     isfound = 0; it = 0
     while isfound < 5 && it < itmax
         it += 1
-        varbayes_lmstep!(hn, Λ0, Δm, As, ks)
+        # Damped Gauss-Newton approximation
+        λ = diagmax(Λn) * μ
         unsafe_copy!(Λf, Λn) # Λf ← Λn
-        cholesky_state = varbayes_lmstep!(mn, ms, hn, Λf, μ)
-
+        @simd for i in eachindex(mn)
+            @inbounds Λf[i,i] += λ # Λf ← Λn + λI
+        end
+        _, cholesky_state = potrf!('L', Λf)
+        # Levenberg-Marquardt step
+        symv!('U', 1.0, Λ0, Δm,  0.0, hn) # hn ← Λ0 * Δm
+        gemv!('T', 1.0, As, ks, -1.0, hn) # hn ← As' * ks - hn
+        trsv!('L', 'N', 'N', Λf, hn)
+        trsv!('L', 'T', 'N', Λf, hn)
+        # Finite-difference approximation for Hessian
+        @simd for i in eachindex(mn)
+            @inbounds mn[i] = ms[i] + α * hn[i]
+        end
+        fcall!(kn, mo, mn)
+        @simd for i in eachindex(kn)
+            @inbounds kn[i] = ys[i] - kn[i] - ks[i]
+        end
+        gemv!('N', invα1, Js, hn, invα2, kn)
+        # Geodesic acceleration step
+        gemv!('T', 1.0, As, kn, 0.0, mn) # use mn for geodesic step
+        trsv!('L', 'N', 'N', Λf, mn)
+        trsv!('L', 'T', 'N', Λf, mn)
+        @simd for i in eachindex(hn)
+            @inbounds hn[i] += mn[i]
+        end
         # compute predicted gain of the free energy by linear approximation
         Ln = 0.5 * (dot(hn, Λn, hn) + μ * dot(hn, hn))
-
         # to peek if the free energy is increased by `hn`
+        @simd for i in eachindex(mn)
+            @inbounds mn[i] = ms[i] + hn[i]
+        end
         fcall!(kn, mo, mn) # use new trial `mn` to compute `kn`
         gcall!(Jn, mo, mn) # use new trial `mn` to compute `Jn`
         varbayes_update!(Λe, Λy, Λ0, Jn, An)
@@ -116,7 +114,7 @@ function varbayes(m0::VecI, Λ0::MatI, ys::VecI, Λy::MatI, mo::Function; τ::Re
         unsafe_copy!(Λf, Λn) # Λf ← Λn
         _, cholesky_state = potrf!('L', Λf)
         Ft = varbayes_energy!(Λy, kn, Λ0, hn, Λf, Λe)
-        ρ  = (Ft - Fn) / Ln
+        ρ  = (Ft - Fn) / Ln # gain ratio
 
         if ρ > 0
             Fn = Ft
@@ -127,10 +125,10 @@ function varbayes(m0::VecI, Λ0::MatI, ys::VecI, Λy::MatI, mo::Function; τ::Re
             unsafe_copy!(As, An)
             unsafe_copy!(Δm, hn)
             varbayes_update!(Λn, Λy, Λ0, Js, As)
-            μ = varbayes_lmdamp(μ, ρ)
+            μ = ρ < 0.9367902323681495 ? μ * (1.0 - cubic(2.0 * ρ - 1.0)) : μ / 3.0
             ν = 2.0; isfound = 0
         else
-            μ = μ * ν; ν = ν * 2.0; isfound += 1
+            μ *= ν; ν *= 2.0; isfound += 1
         end
     end
     return ms, Λs
