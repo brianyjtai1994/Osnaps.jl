339baa748cf4a941b273017287d4c6d38ee9fe50
yctai <brianyjtai1994@gmail.com>
2021-09-14 00:05:28 +0800

    Add: Predictive distribution, new test for variational inference.

--- src/Bayes/variantional_bayesian.jl
+++ src/Bayes/variantional_bayesian.jl
@@ -1,44 +1,66 @@
-struct VarInference <: AbstractMinimizer
-    @def prop Vector{Float64} xs rs rc θs θc Δθ dθ δθ
-    @def prop Matrix{Float64} Σs Js Jc As Ac Λs Λc Λt Λf
-    @def prop Int ny nd
+struct VarInfOptimizer <: AbstractOptimizer
+    @def prop Vector{Float64} rs rc μs μc Δμ dμ δμ
+    @def prop Matrix{Float64} Js Jc As Ac Λs Λc Λt Λe Λf
+    @def prop Int nd ny
 
-    function VarInference(nd::Int, ny::Int)
+    function VarInfOptimizer(nd::Int, ny::Int)
         @def vars Vector{Float64}(undef, ny) rs rc
-        @def vars Vector{Float64}(undef, nd) xs θs θc Δθ dθ δθ
+        @def vars Vector{Float64}(undef, nd) μs μc Δμ dμ δμ
         @def vars Matrix{Float64}(undef, ny, nd) Js Jc As Ac
-        @def vars Matrix{Float64}(undef, nd, nd) Σs Λs Λc Λt Λf
-        return new(xs, rs, rc, θs, θc, Δθ, dθ, δθ, Σs, Js, Jc, As, Ac, Λs, Λc, Λt, Λf, ny, nd)
+        @def vars Matrix{Float64}(undef, nd, nd) Λs Λc Λt Λe Λf
+        return new(rs, rc, μs, μc, Δμ, dμ, δμ, Js, Jc, As, Ac, Λs, Λc, Λt, Λe, Λf, nd, ny)
     end
 end
 
-Base.show(io::IO, o::VarInference) = print(io, "Variational Inference Optimizer(nd = $(o.nd), ny = $(o.ny))")
+Base.show(io::IO, o::VarInfOptimizer) = print(io, "Variational Inference Optimizer(nd = $(o.nd), ny = $(o.ny))")
 
-function minimize!(o::VarInference, fn::Function, θ0::VecI, Λ0::MatI, x::VecI, y::VecI, Λy::MatI, τ::Real, h::Real, itmax::Int)
+struct InferredPosterior
+    @def prop Vector{Float64} μ g
+    @def prop Matrix{Float64} Σ Λ
+
+    function InferredPosterior(o::VarInfOptimizer)
+        @get o nd μs Λs Λf
+        @def vars Vector{Float64}(undef, nd)     μ g
+        @def vars Matrix{Float64}(undef, nd, nd) Σ Λ
+        @copy!(μs => μ, Λs => Λ, Λs => Λf)
+        for j in eachindex(μ)
+            @simd for i in eachindex(μ)
+                @inbounds Σ[i,j] = ifelse(i == j, 1.0, 0.0)
+            end
+        end
+        potrf!('L', Λf)
+        trsm!('L', 'L', 'N', 'N', 1.0, Λf, Σ)
+        trsm!('L', 'L', 'T', 'N', 1.0, Λf, Σ)
+        return new(μ, g, Σ, Λ)
+    end
+end
+
+Base.show(io::IO, o::InferredPosterior) = print(io, "Inferred Posterior of Variational Inference (dim = $(length(o.μ)))")
+
+function inference!(o::VarInfOptimizer, fn::Function, μ0::VecI, Λ0::MatI, x::VecI, y::VecI, Λy::MatI, τ::Real, h::Real, itmax::Int)
     #### Allocations ####
-    @get o nd ny
-    @get o xs rs rc θs θc Δθ dθ δθ
-    @get o Σs Js Jc As Ac Λs Λc Λt Λf
+    @get o nd ny rs rc μs μc Δμ dμ δμ
+    @get o Js Jc As Ac Λs Λc Λt Λe Λf
     #### Initialization
     invh1  = inv(h)
     invh2  = invh1 * invh1
     one2nd = eachindex(1:nd)
-    @copy!(θ0 => θs, Λ0 => Λs)
+    @copy!(μ0 => μs, Λ0 => Λs)
     #### Compute precision matrix
-    rsd!(rs, fn, θs; x, y)
-    jac!(Js, fn, θs; x)
+    rsd!(rs, fn, μs; x, y)
+    jac!(Js, fn, μs; x)
     @copy!(Λ0 => Λc)
     symm!('L', 'U', 1.0, Λy, Js, 0.0, As) # As ← Λy  * Js
     gemm!('T', 'N', 1.0, As, Js, 1.0, Λc) # Λc ← As' * Js + Λ0
     μ = diagmax(Λc, nd) * τ
     ν = 2.0
     #### Compute free energy
-    fill!(Δθ, 0.0)
-    @copy!(Λs => Λf, Λc => Σs)
+    fill!(Δμ, 0.0)
+    @copy!(Λs => Λf, Λc => Λe)
     _, cholesky_state = potrf!('L', Λf)
-    trsm!('L', 'L', 'N', 'N', 1.0, Λf, Σs)
-    trsm!('L', 'L', 'T', 'N', 1.0, Λf, Σs)
-    Fnow = 0.5 * (dot(rs, ny, Λy, rs, ny) + tr(Σs, nd)) + logdet(Λf, nd)
+    trsm!('L', 'L', 'N', 'N', 1.0, Λf, Λe)
+    trsm!('L', 'L', 'T', 'N', 1.0, Λf, Λe)
+    Fnow = 0.5 * (dot(rs, ny, Λy, rs, ny) + tr(Λe, nd)) + logdet(Λf, nd)
     #### Iteration
     it = 0
     while it < itmax
@@ -51,51 +73,51 @@ function minimize!(o::VarInference, fn::Function, θ0::VecI, Λ0::MatI, x::VecI,
         end
         _, cholesky_state = potrf!('L', Λf)
         # Levenberg-Marquardt step by 1st order linearization
-        symv!('U', 1.0, Λ0, Δθ,  0.0, dθ) # dθ ← Λ0  * Δθ
-        gemv!('T', 1.0, As, rs, -1.0, dθ) # dθ ← As' * rs - dθ
-        trsv!('L', 'N', 'N', Λf, dθ)
-        trsv!('L', 'T', 'N', Λf, dθ)
+        symv!('U', 1.0, Λ0, Δμ,  0.0, dμ) # dμ ← Λ0  * Δμ
+        gemv!('T', 1.0, As, rs, -1.0, dμ) # dμ ← As' * rs - dμ
+        trsv!('L', 'N', 'N', Λf, dμ)
+        trsv!('L', 'T', 'N', Λf, dμ)
         # Finite-difference of 2nd order directional derivative
         @simd for i in one2nd
-            @inbounds θc[i] = θs[i] + h * dθ[i]
+            @inbounds μc[i] = μs[i] + h * dμ[i]
         end
-        rsd!(rc, fn, θc; x, y)
+        rsd!(rc, fn, μc; x, y)
         axpy!(-1.0, rs, rc)
-        gemv!('N', invh1, Js, dθ, invh2, rc)
+        gemv!('N', invh1, Js, dμ, invh2, rc)
         # Geodesic acceleration step by 2nd order linearization
-        gemv!('T', 1.0, As, rc, 0.0, δθ)
-        trsv!('L', 'N', 'N', Λf, δθ)
-        trsv!('L', 'T', 'N', Λf, δθ)
-        gratio = 2.0 * nrm2(δθ) / nrm2(dθ)
+        gemv!('T', 1.0, As, rc, 0.0, δμ)
+        trsv!('L', 'N', 'N', Λf, δμ)
+        trsv!('L', 'T', 'N', Λf, δμ)
+        gratio = 2.0 * nrm2(δμ) / nrm2(dμ)
         gratio > 0.75 && (μ *= ν; ν *= 2.0; continue)
         # (Levenberg-Marquardt velocity) + (Geodesic acceleration)
-        axpy!(1.0, δθ, dθ)
+        axpy!(1.0, δμ, dμ)
         # Compute predicted gain of the free energy by linear approximation
-        LinApprox = 0.5 * dot(dθ, nd, Λc, dθ, nd) + μ * dot(dθ, dθ, nd)
+        LinApprox = 0.5 * dot(dμ, nd, Λc, dμ, nd) + μ * dot(dμ, dμ, nd)
         # Compute gain ratio
         @simd for i in one2nd
-            @inbounds θc[i] = θs[i] + dθ[i]
+            @inbounds μc[i] = μs[i] + dμ[i]
         end
-        rsd!(rc, fn, θc; x, y)
-        jac!(Jc, fn, θc; x)
+        rsd!(rc, fn, μc; x, y)
+        jac!(Jc, fn, μc; x)
         @copy!(Λ0 => Λt)
         symm!('L', 'U', 1.0, Λy, Jc, 0.0, Ac) # Ac ← Λy  * Jc
         gemm!('T', 'N', 1.0, Ac, Jc, 1.0, Λt) # Λt ← Ac' * Jc + Λ0
         @simd for i in one2nd
-            @inbounds δθ[i] = θc[i] - θ0[i]
+            @inbounds δμ[i] = μc[i] - μ0[i]
         end
-        @copy!(Λc => Λf, Λt => Σs)
+        @copy!(Λc => Λf, Λt => Λe)
         _, cholesky_state = potrf!('L', Λf)
-        trsm!('L', 'L', 'N', 'N', 1.0, Λf, Σs)
-        trsm!('L', 'L', 'T', 'N', 1.0, Λf, Σs)
-        Fnew = 0.5 * (dot(rc, ny, Λy, rc, ny) + dot(δθ, nd, Λ0, δθ, nd) + tr(Σs, nd)) + logdet(Λf, nd)
+        trsm!('L', 'L', 'N', 'N', 1.0, Λf, Λe)
+        trsm!('L', 'L', 'T', 'N', 1.0, Λf, Λe)
+        Fnew = 0.5 * (dot(rc, ny, Λy, rc, ny) + dot(δμ, nd, Λ0, δμ, nd) + tr(Λe, nd)) + logdet(Λf, nd)
 
         Δ = Fnow - Fnew; ρ = Δ / LinApprox
         if ρ > 0.0
             Fnow = Fnew
-            @copy!(θc => θs, Λc => Λs)
-            (maximum(δθ) < 1e-16 || Δ < 1e-10) && break # Check localized convergence
-            @copy!(rc => rs, Λt => Λc, Jc => Js, Ac => As, δθ => Δθ)
+            @copy!(rc => rs, μc => μs, Λc => Λs, Jc => Js, Ac => As)
+            (maximum(δμ) < 1e-16 || Δ < 1e-10) && break # Check localized convergence
+            @copy!(Λt => Λc, δμ => Δμ)
             μ = ρ < 0.9367902323681495 ? μ * (1.0 - cubic(2.0 * ρ - 1.0)) : μ / 3.0
             ν = 2.0
         else
@@ -103,13 +125,14 @@ function minimize!(o::VarInference, fn::Function, θ0::VecI, Λ0::MatI, x::VecI,
             μ *= ν; ν *= 2.0
         end
     end
-    # compute solution parameters and covariance
-    @copy!(θs => xs)
-    fill!(Σs, 0.0)
-    @simd for i in one2nd
-        @inbounds Σs[i,i] = 1.0
+    return InferredPosterior(o)
+end
+
+function inference!(y::VecIO, yerr::VecIO, f::Function, o::InferredPosterior, σ::Real; x::VecI)
+    @get o μ g Σ
+    fnc!(y, f, μ; x)
+    @inbounds for i in eachindex(yerr)
+        jac!(g, f, μ; x=x[i])
+        yerr[i] = sqrt(σ + dot(g, Σ, g))
     end
-    trsm!('L', 'L', 'N', 'N', 1.0, Λf, Σs)
-    trsm!('L', 'L', 'T', 'N', 1.0, Λf, Σs)
-    return nothing
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,16 @@
 using Osnaps, Test
 
-print_head(s::String) = println(" \033[1m\033[32mTest Osnaps\033[0m \033[33m" * s * "\033[0m")
+print_head(s::String) = println("\n \033[1m\033[32mTest Osnaps\033[0m \033[33m" * s * "\033[0m")
 print_body(s::String) = println(" "^13 * s)
 
 @testset "Variational Inference: NIST Dataset(BoxBOD)" begin
     X = [1., 2., 3., 5., 7., 10.]
     Y = [109., 149., 149., 191., 213., 224.]
     Λ = reshape([ifelse(i == j, 1.0, 0.0) for i in 1:6, j in 1:6], (6, 6))
-    o = minimizer(2; ny=6, method="varinf")
+    o = optimizer(2; ny=6, method="varinf")
 
     fn(x::Real, θ1::Real, θ2::Real) = θ1 * (1.0 - exp(-θ2 * x))
-    fn(x::Real, θ::AbstractVector)  = fn(x, θ[1], θ[2])
-
-    function Osnaps.fnc!(y::AbstractVector, f::typeof(fn), θ::AbstractVector; x::AbstractVector)
-        @inbounds for i in eachindex(y)
-            y[i] = f(x[i], θ)
-        end
-        return nothing
-    end
-
-    function Osnaps.rsd!(r::AbstractVector, f::typeof(fn), θ::AbstractVector; x::AbstractVector, y::AbstractVector)
-        Osnaps.fnc!(r, f, θ; x)
-        @simd for i in eachindex(r)
-            @inbounds r[i] = y[i] - r[i]
-        end
-        return nothing
-    end
+    fn(x::Real, θ::AbstractVector)  = @inbounds fn(x, θ[1], θ[2])
 
     function Osnaps.jac!(J::AbstractMatrix, f::typeof(fn), θ::AbstractVector; x::AbstractVector)
         @inbounds θ1 = θ[1]
@@ -38,12 +23,58 @@ print_body(s::String) = println(" "^13 * s)
         return nothing
     end
 
-    minimize!(o, fn, [10., 5.5], [1e-5 0.; 0. 1e-5], X, Y, Λ)
+    p = inference!(o, fn, [10., 5.5], reshape([ifelse(i == j, 1e-5, 0.0) for i in 1:2, j in 1:2], (2, 2)), X, Y, Λ)
     print_head("Variational Inference: NIST Dataset(BoxBOD)")
-    print_body("xsol[1] = $(round(o.xs[1],       digits=3))")
-    print_body("xsol[2] = $(round(o.xs[2],       digits=3))")
-    print_body("xerr[1] = $(round(sqrt(o.Σs[1]), digits=3))")
-    print_body("xerr[2] = $(round(sqrt(o.Σs[4]), digits=3))")
-    @test o.xs[1] ≈ 213.80 atol=0.01
-    @test o.xs[2] ≈   0.55 atol=0.01
+    print_body("xsol[1] = $(round(p.μ[1],       digits=3))")
+    print_body("xsol[2] = $(round(p.μ[2],       digits=3))")
+    print_body("xerr[1] = $(round(sqrt(p.Σ[1]), digits=3))")
+    print_body("xerr[2] = $(round(sqrt(p.Σ[4]), digits=3))")
+    @test p.μ[1] ≈ 213.80 atol=0.01
+    @test p.μ[2] ≈   0.55 atol=0.01
+end
+
+@testset "Variational Inference: NIST Dataset(Eckerle4)" begin
+    X = [400.0, 405.0, 410.0, 415.0, 420.0, 425.0, 430.0,
+         435.0, 436.5, 438.0, 439.5, 441.0, 442.5, 444.0,
+         445.5, 447.0, 448.5, 450.0, 451.5, 453.0, 454.5,
+         456.0, 457.5, 459.0, 460.5, 462.0, 463.5, 465.0,
+         470.0, 475.0, 480.0, 485.0, 490.0, 495.0, 500.0]
+    Y = [1.575000e-04, 1.699000e-04, 2.350000e-04, 3.102000e-04, 4.917000e-04,
+         8.710000e-04, 1.741800e-03, 4.640000e-03, 6.589500e-03, 9.730200e-03,
+         1.490020e-02, 2.373100e-02, 4.016830e-02, 7.125590e-02, 1.264458e-01,
+         2.073413e-01, 2.902366e-01, 3.445623e-01, 3.698049e-01, 3.668534e-01,
+         3.106727e-01, 2.078154e-01, 1.164354e-01, 6.167640e-02, 3.372000e-02,
+         1.940230e-02, 1.178310e-02, 7.435700e-03, 2.273200e-03, 8.800000e-04,
+         4.579000e-04, 2.345000e-04, 1.586000e-04, 1.143000e-04, 7.100000e-05]
+    Λ = reshape([ifelse(i == j, 1.0, 0.0) for i in 1:35, j in 1:35], (35, 35))
+    o = optimizer(3; ny=35, method="varinf")
+
+    fn(x::Real, θ1::Real, θ2::Real, θ3::Real) = θ1 * exp(-0.5 * abs2((x - θ3) / θ2)) / θ2
+    fn(x::Real, θ::AbstractVector)            = @inbounds fn(x, θ[1], θ[2], θ[3])
+
+    function Osnaps.jac!(J::AbstractMatrix, f::typeof(fn), θ::AbstractVector; x::AbstractVector)
+        @inbounds θ1 = θ[1]
+        @inbounds θ2 = θ[2]
+        @inbounds θ3 = θ[3]
+        @inbounds for i in eachindex(x)
+            tmp1 = abs2(x[i] - θ3) / abs2(θ2)
+            tmp2 = exp(-0.5 * tmp1)
+            J[i,1] = tmp2 / θ2
+            J[i,2] = θ1 * tmp2 * (tmp1 - 1.0) / abs2(θ2)
+            J[i,3] = θ1 * (x[i] - θ3) * tmp2 / Osnaps.cubic(θ2)
+        end
+        return nothing
+    end
+
+    p = inference!(o, fn, [1.5, 5.0, 450.0], reshape([ifelse(i == j, 1e-5, 0.0) for i in 1:3, j in 1:3], (3, 3)), X, Y, Λ)
+    print_head("Variational Inference: NIST Dataset(Eckerle4)")
+    print_body("xsol[1] = $(round(p.μ[1],       digits=3))")
+    print_body("xsol[2] = $(round(p.μ[2],       digits=3))")
+    print_body("xsol[3] = $(round(p.μ[3],       digits=3))")
+    print_body("xerr[1] = $(round(sqrt(p.Σ[1]), digits=3))")
+    print_body("xerr[2] = $(round(sqrt(p.Σ[5]), digits=3))")
+    print_body("xerr[3] = $(round(sqrt(p.Σ[9]), digits=3))")
+    @test p.μ[1] ≈   1.554 atol=0.005
+    @test p.μ[2] ≈   4.088 atol=0.005
+    @test p.μ[3] ≈ 451.541 atol=0.005
 end
